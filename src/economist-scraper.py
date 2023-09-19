9f71b7a1e47bcafd03e863267297c82cf2d437f0
Stephanie W Franco <talia1984@yahoo.com>
2023-09-19 21:35:05 +0200

    Update

--- src/economist-scraper.py
+++ src/economist-scraper.py
@@ -3,15 +3,22 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.15.1
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %% language="sh"
+# which python3
+
+# %%
+print("ciao")
+
+# %%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,6 +26,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException, NoSuchElementException
 from selenium.webdriver.support.select import Select
 
+# %%
 from pathlib import Path
 import subprocess
 from natsort import natsorted
@@ -39,9 +47,10 @@ import typing
 from borb.pdf import Document
 from borb.pdf import PDF
 
+# %% [markdown]
 # # Config
 
-# +
+# %%
 proquest_url = "https://www.proquest.com/publication/41716"
 
 browser_app = "firefox"
@@ -49,7 +58,7 @@ geckodriver_path = Path(os.environ["HOME"])/".local"/"bin"/"geckodriver"
 assert(geckodriver_path.is_file())
 headless_browser = False
 
-continue_download = True
+continue_download = False
 
 datadir = Path("../data").resolve()
 downloaddir = datadir/"download"
@@ -64,15 +73,18 @@ journal_month = None
 # lower numbers correspond to more recent issues
 journal_issue = None
 
-sleep_time = (10, 20)
-# -
+sleep_time = (5, 15)
 
-datadir.mkdir(parents=True, exist_ok=True)
+# %%
+assert(datadir.is_dir())
+if downloaddir.is_dir() and (not continue_download):
+    shutil.rmtree(downloaddir)
 downloaddir.mkdir(parents=True, exist_ok=continue_download)
 artdir.mkdir(parents=True, exist_ok=True)
 pagesdir.mkdir(parents=True, exist_ok=True)
 tocdir.mkdir(parents=True, exist_ok=True)
 
+# %%
 if(journal_latest):
     assert(journal_year is None)
     assert(journal_month is None)
@@ -93,8 +105,10 @@ if(journal_year or journal_month or journal_issue):
     assert(0<=journal_issue<=3)
 
 
+# %% [markdown]
 # # Functions
 
+# %%
 def get_browser(browser_app="firefox", headless_browser=False, geckodriver_path=None):
     browser = None
     if browser_app == "firefox":
@@ -118,6 +132,7 @@ def get_browser(browser_app="firefox", headless_browser=False, geckodriver_path=
     return browser
 
 
+# %%
 def check_captcha(browser):
     try:
         browser.find_element(By.CSS_SELECTOR, "form#verifyCaptcha")
@@ -127,6 +142,7 @@ def check_captcha(browser):
     return True
 
 
+# %%
 def select_issue(journal_year, journal_month, journal_issue):
     # Select year
     try:
@@ -160,26 +176,40 @@ def select_issue(journal_year, journal_month, journal_issue):
     time.sleep(5)
 
 
+# %% [markdown]
 # # Main
 
-logging.basicConfig()
+# %%
+logging.basicConfig(force=True, level=logging.INFO,
+                   format="[%(asctime)s] [%(filename)s]->[%(funcName)s] [%(levelname)s] %(message)s")
 
+# %%
+logging.info("Start")
+
+# %% [markdown]
 # Open web browser
 
+# %%
 browser = get_browser(browser_app, headless_browser)
 
+# %% [markdown]
 # DELETE THIS CELL
 
+# %%
 from mylogin import mylogin
 mylogin(browser, datadir)
 
+# %% [markdown]
 # Connect to ProQuest website
 
+# %%
 browser.get(proquest_url)
 time.sleep(5)
 
+# %% [markdown]
 # Reject cookies
 
+# %%
 css = "button#onetrust-reject-all-handler"
 try:
     el = browser.find_element(By.CSS_SELECTOR, css)
@@ -189,13 +219,17 @@ else:
     el.click()
     time.sleep(3)
 
+# %% [markdown]
 # Select issue to download
 
+# %%
 if not journal_latest:
     select_issue(journal_year, journal_month, journal_issue)
 
+# %% [markdown]
 # Get number of articles to download
 
+# %%
 while True:
     max_len = len(list(browser.find_elements(By.CSS_SELECTOR, "li.resultItem.ltr")))
     logging.info(f"max_len={max_len}")
@@ -208,22 +242,35 @@ while True:
     else:
         break
 
+# %% [markdown]
 # Get date in which the issue was released
 
+# %%
 issue_date = browser.find_element(By.CSS_SELECTOR, "select#issueSelected")
 issue_date = issue_date.text
 issue_date = issue_date.split(";")[0].strip()
 issue_date = datetime.datetime.strptime(issue_date, '%b %d, %Y').date()
 logging.info(f"issue_date={issue_date}")
 
+# %% [markdown]
+# Build final file path and check we have not downloaded this issue yet
+
+# %%
+ts = issue_date.strftime("%Y-%m-%d")
+finalfp = datadir/"final"/("TheEconomist-"+ts+".pdf")
+assert(not finalfp.is_file())
+
+# %% [markdown]
 # Download list of articles
 
+# %%
 all_titles = list()
 all_pages = list()
 all_pdfurls = list()
 toc = False
 result_items = list(browser.find_elements(By.CSS_SELECTOR, "li.resultItem.ltr"))
 
+# %%
 for result_item in tqdm(result_items):
     
     title = result_item.find_element(By.CSS_SELECTOR, "div.truncatedResultsTitle").text.strip()
@@ -248,8 +295,10 @@ for result_item in tqdm(result_items):
     all_pages.append(pages)
     all_pdfurls.append(pdfurl)
 
+# %% [markdown]
 # Downloads single articles
 
+# %%
 for pdfi, pdfurl in tqdm(enumerate(all_pdfurls), total=len(all_pdfurls)):
 
     # Get page numbers
@@ -289,12 +338,16 @@ for pdfi, pdfurl in tqdm(enumerate(all_pdfurls), total=len(all_pdfurls)):
     # Sleep to prevent captcha
     time.sleep(random.uniform(*sleep_time))
 
+# %% [markdown]
 # Close web browser
 
+# %%
 browser.close()
 
+# %% [markdown]
 # Get size of a PDF page
 
+# %%
 # read PDF
 fn = next(artdir.iterdir())
 logging.info(f"Using fn='{fn}'")
@@ -308,12 +361,15 @@ w = doc.get_page(0).get_page_info().get_width()
 h = doc.get_page(0).get_page_info().get_height()
 pdf_page_size = [w,h]
 
+# %%
 logging.info(f"Page width={pdf_page_size[0]}; Page height={pdf_page_size[1]}")
 logging.info(f"Page width={type(pdf_page_size[0])}; Page height={type(pdf_page_size[1])}")
 logging.info(f"Page width={str(pdf_page_size[0])}; Page height={str(pdf_page_size[1])}")
 
+# %% [markdown]
 # If we have TOC, get page number and rename
 
+# %%
 if toc:
     tocpdf = tocdir/("pages_toc.pdf")
     assert(tocpdf.is_file())
@@ -338,8 +394,10 @@ if toc:
     pdffn = artdir/("pages_"+this_pages+".pdf")
     shutil.copy(tocpdf, pdffn)
 
+# %% [markdown]
 # Generate TOC if we don't have one
 
+# %%
 if not toc:
     tocmdfile = tocdir/"toc.md"
     tocpdffile = tocmdfile.parent/(tocmdfile.stem+".pdf")
@@ -359,8 +417,10 @@ if not toc:
     assert(ret.returncode==0)
     shutil.move(tocpdffile, artdir/"pages_2-3.pdf")
 
+# %% [markdown]
 # Split pages
 
+# %%
 pagesfns = natsorted(list(artdir.iterdir()))
 for pagesfn in pagesfns:
     this_pages = re.search(".*?pages_(.*)\.pdf", str(pagesfn)).group(1)
@@ -372,8 +432,10 @@ for pagesfn in pagesfns:
         ret = subprocess.run(cmd)
         assert(ret.returncode==0)
 
+# %% [markdown]
 # Generate white page
 
+# %%
 whitepdf = downloaddir/"blank.pdf"
 cmd = ["convert",
        list(artdir.iterdir())[1],
@@ -382,8 +444,10 @@ cmd = ["convert",
 ret = subprocess.run(cmd)
 assert(ret.returncode==0)
 
+# %% [markdown]
 # Put white pages where needed
 
+# %%
 pages_we_have = [str(x) for x in artdir.iterdir()]
 pages_we_have = [re.search(".*?pages_(.*)\.pdf$",x).group(1).split("-") for x in pages_we_have]
 pages_we_have = list(itertools.chain(*pages_we_have))
@@ -395,9 +459,10 @@ for i in range(1,max(pages_we_have)+1):
         filename = ("pages_"+str(i)+".pdf")
         shutil.copy(whitepdf, pagesdir/filename)
 
+# %% [markdown]
 # Download cover
 
-# +
+# %%
 ts = issue_date.strftime("%Y%m%d")
 url = f"https://www.economist.com/img/b/1280/1684/90/media-assets/image/{ts}_DE_EU.jpg"
 
@@ -405,7 +470,7 @@ data = requests.get(url).content
 with open(downloaddir/"cover.jpg","wb") as fh:
     fh.write(data)
 
-# +
+# %%
 #pt_to_inch = 0.0138888889
 #pdf_page_size_pix = list()
 #for m in pdf_page_size:
@@ -413,11 +478,11 @@ with open(downloaddir/"cover.jpg","wb") as fh:
 #    pdf_page_size_pix.append(m2)
 #pages_we_have(pdf_page_size)
 #pages_we_have(pdf_page_size_pix)
-# -
 
+# %% [markdown]
 # Convert cover from JPG to PDF
 
-# +
+# %%
 page1fn = (pagesdir/"pages_1.pdf")
 if page1fn.is_file():
     page1fn.unlink()
@@ -426,10 +491,11 @@ cmd = ["magick", "convert", downloaddir/"cover.jpg", "-page", str(w)+"x"+str(h)+
 logging.info(f"Command to convert cover from JPG to PDF: '{cmd}'")
 ret = subprocess.run(cmd)
 assert(ret.returncode==0)
-# -
 
+# %% [markdown]
 # Merge pages
 
+# %%
 cmd = ["qpdf", "--empty", "--pages"]
 cmd += [str(x) for x in natsorted(pagesdir.iterdir())]
 cmd += ["--", str(downloaddir/"output.pdf")]
@@ -437,31 +503,37 @@ logging.info(f"Command to merge pages: '{cmd}'")
 ret = subprocess.run(cmd)
 assert(ret.returncode==0)
 
+# %% [markdown]
 # Insert bookmarks
 
+# %%
 bookmarkfn = downloaddir/"bookmark.txt"
 with open(bookmarkfn, "w") as fh:
     for titlei, title in enumerate(all_titles):
         fh.write("+\""+title+"\"|"+all_pages[titlei].split("-")[0]+"\n")
 
+# %%
 # def run_script(pdf_in_filename, bookmarks_filename, pdf_out_filename=None):
 infn = downloaddir/"output.pdf"
 outfn = downloaddir/"output_bookmarked.pdf"
 pdfbookmarker.run_script(str(infn), str(bookmarkfn), str(outfn))
 infn.unlink()
 
+# %% [markdown]
 # Compress
 
+# %%
 infn = outfn
 outfn = downloaddir/"output_bookmarked_compressed.pdf"
 cmd = ["ps2pdf", "-dPDFSETTINGS=/ebook", str(infn), str(outfn)]
 ret = subprocess.run(cmd)
 assert(ret.returncode==0)
 
+# %% [markdown]
 # Move PDF to "final" subfolder
 
-ts = issue_date.strftime("%Y-%m-%d")
-finalfp = datadir/"final"/(ts+".pdf")
+# %%
 shutil.move(outfn, finalfp)
 
-
+# %%
+logging.info("Ended")
