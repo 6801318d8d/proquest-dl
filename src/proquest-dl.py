f1da6e2ab0606f208122d0bdb345d0fb8cdaa996
Stephanie W Franco <talia1984@yahoo.com>
2024-04-24 12:06:29 +0200

    Add shebanhs

--- src/proquest-dl.py
+++ src/proquest-dl.py
@@ -1,21 +1,7 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.15.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# %% [markdown]
-# # Imports
+#!/usr/bin/env python3
 
 # %%
+# Imports
 import datetime
 import itertools
 import json
